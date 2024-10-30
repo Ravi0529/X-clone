dc0fa5aabf0857e19585dd9f8c251b44180ce8a9
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2024-10-31 00:55:50 +0530

    user-controllers-added

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js"
+import bcrypt from "bcryptjs"
 
 export const signup = async (req, res) => {
     try {
@@ -21,11 +22,14 @@ export const signup = async (req, res) => {
             });
         }
 
+        const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
         const newUser = new User({
             fullName,
             username,
             email,
-            password
+            password: hashedPassword
         })
 
         if (newUser) {
@@ -61,11 +65,11 @@ export const login = async (req, res) => {
             message: "User not found"
         })
 
-        const isMatch = await userInfo.comparePassword(password)
-        if (!isMatch) return res.status(400).json({
-            error: true,
-            message: "Incorrect Password"
-        })
+        const isPasswordCorrect = await bcrypt.compare(password, userInfo?.password || "")
+
+		if (!userInfo || !isPasswordCorrect) {
+			return res.status(400).json({ error: "Invalid username or password" });
+		}
 
         generateTokenAndSetCookie(userInfo._id, res);
 
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -64,19 +64,6 @@ const userSchema = new mongoose.Schema(
     { timestamps: true }
 )
 
-// hash password before saving
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next()
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-    next()
-})
-
-// method to compare password for login
-userSchema.methods.comparePassword = async function (candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.password)
-}
-
 const User = mongoose.model("User", userSchema)
 
 export default User
