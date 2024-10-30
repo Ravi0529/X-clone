a430012d14af3769aaab48d7844d5c84f8c807d3
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2024-10-30 15:38:40 +0530

    backend-login-logout

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -33,20 +33,66 @@ export const signup = async (req, res) => {
             await newUser.save()
 
             res.status(201).json({ newUser })
-            // res.status(201).json({
-            //     _id: newUser._id,
-            //     fullName: newUser.fullName,
-            //     username: newUser.username,
-            //     email: newUser.email,
-            //     followers: newUser.followers,
-            //     following: newUser.following,
-            //     profileImg: newUser.profileImg,
-            //     coverImg: newUser.coverImg
-            // })
         }
         else res.status(400).json({ error: "Invalid user data" })
     }
     catch (error) {
-        res.status(500).json({error: "Internal Server Error"})
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body
+
+        if (!username) return res.status(400).json({
+            error: true,
+            message: "Username is required"
+        })
+
+        if (!password) return res.status(400).json({
+            error: true,
+            message: "Password is required"
+        })
+
+        const userInfo = await User.findOne({ username })
+        if (!userInfo) return res.status(400).json({
+            error: true,
+            message: "User not found"
+        })
+
+        const isMatch = await userInfo.comparePassword(password)
+        if (!isMatch) return res.status(400).json({
+            error: true,
+            message: "Incorrect Password"
+        })
+
+        generateTokenAndSetCookie(userInfo._id, res);
+
+        res.status(201).json({
+            error: false,
+            message: "Login Successful",
+            _id: userInfo._id,
+            fullName: userInfo.fullName,
+            username: userInfo.username,
+            email: userInfo.email,
+            followers: userInfo.followers,
+            following: userInfo.following,
+            profileImg: userInfo.profileImg,
+            coverImg: userInfo.coverImg
+        })
+    }
+    catch (error) {
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+}
+
+export const logout = async (_, res) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 0 })
+        res.status(200).json({ message: "Logout Successfully" })
+    }
+    catch (error) {
+        res.status(500).json({ error: "Internal Server Error" })
     }
 }
