60fbbf1c86075ef68c6ecdb8a6972e0643b9fc9f
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2024-11-03 23:39:16 +0530

    notification-controller-added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import postRoutes from "./routes/post.routes.js"
+import notificationRoutes from "./routes/notification.routes.js"
 import connectMongoDB from "./db/connectMongoDB.js"
 import cookieParser from "cookie-parser"
 import { v2 as cloudinary } from "cloudinary"
@@ -28,6 +29,7 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
+app.use("/api/notifications", notificationRoutes)
 
 app.listen(PORT, () => {
     connectMongoDB()
